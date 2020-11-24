e845fd2f27e97dc77ea433f41e92364b9b69267e
Samarth Patel <samarthxpatel@gmail.com>
2020-11-24 15:10:11 -0500

    Finished landing page; Started search results page

---
+++ results/index.js
@@ -0,0 +1,12 @@
+const search = document.getElementById('d1')
+const submit = document.getElementById('d2')
+
+search.addEventListener('keyup', (event) => {
+	event.preventDefault()
+	if (event.keyCode === 13) {
+		submit.focus()
+		submit.click()
+	}
+})
+
+submit.onclick = () => console.log('here')
