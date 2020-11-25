5e5a95d1ddff099c323970cd0ae9afd90dad5c8d
Samarth Patel <samarthxpatel@gmail.com>
2020-11-25 12:59:44 -0500

    Finished Login page

---
+++ login/index.js
@@ -0,0 +1,14 @@
+const search = document.getElementById('d1')
+const submit = document.getElementById('d2')
+
+search.addEventListener('keyup', (event) => {
+	event.preventDefault()
+	if (event.keyCode === 13) {
+		submit.focus()
+		submit.click()
+	}
+})
+
+submit.onclick = () => {
+	window.location.href = '/results/?search=' + 'asdf'
+}
